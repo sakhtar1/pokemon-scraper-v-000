7e8cdf0a2f5eb4fa4031e047ad11d4f8c8987674
Sharmina Akhtar <sakht318@gmail.com>
2018-02-14 17:49:05 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+
+  @@all = []
+  def self.initialize(name,type,db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save
+    @db.execute("INSERT INTO pokemon (id, name, type) VALUES (1, 'pikachu', 'electric')",id, name, type)
+  end
+
+  def self.find
+
+
 end
